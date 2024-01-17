a95bff5e69499a270a1555a8cbdc44b1e4a1f8a7
mhossain2 <moinulhossainmahim@gmail.com>
2024-01-17 14:48:35 +0600

    added success and error response

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -2,15 +2,25 @@ import axios from "axios"
 import { useQuery } from "react-query"
 
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes');
+  return axios.get('http://localhost:4000/superheroe1s');
 }
 
 export const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log('successfully fetched', data);
+  }
+
+  const onError = (data) => {
+    console.log('Getting error', data);
+  }
+
   const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
       enabled: false,
+      onSuccess,
+      onError,
     });
 
   console.log({ isLoading, isFetching });
